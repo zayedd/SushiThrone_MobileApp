5513e2726bf0c8b61221a6001488b24f8b3dc9ba
Araouf17 <76711991+Araouf17@users.noreply.github.com>
2021-12-18 22:55:43 +0200

    added user model

---
+++ sushi_throne/lib/models/user_model.dart
@@ -0,0 +1,12 @@
+class UserModel {
+  final String userName;
+  final String userEmail;
+  final String userImage;
+  
+
+  UserModel(
+    this.userEmail,
+    this.userImage,
+    this.userName,
+  );
+}
